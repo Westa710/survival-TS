49f194ba1b303f132aed38d1c6a2967ad7ae149e
ko <pleiades1424@gmail.com>
2025-02-08 15:16:08 +0900

    feat: 猫画像描画アプリの実装

--- random-cat/pages/index.tsx
+++ random-cat/pages/index.tsx
@@ -1,14 +1,47 @@
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
+import { useEffect, useState } from "react";
+import styles from "./index.module.css";
 
-const IndexPage: NextPage = () => {
-  return <div>猫画像予定地</div>;
+type Props = {
+  initialImageUrl: string;
+};
+
+const IndexPage: NextPage<Props> = ({initialImageUrl}) => {
+  const [imageUrl, setImageUrl] = useState(initialImageUrl);
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = async() => {
+    setLoading(true);
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url);
+    setLoading(false);
+  }
+
+  return (
+    <div className={styles.page}>
+      <button onClick={handleClick} className={styles.button}>他のにゃんこも見る</button>
+      <div className={styles.frame}>{loading || <img src={imageUrl} />}</div>
+    </div>
+  );
 };
 export default IndexPage;
 
-const fetchImage = async () => {
+export const getServerSideProps: GetServerSideProps<Props> = async() => {
+  const image = await fetchImage();
+  return {
+    props: {
+      initialImageUrl: image.url,
+    },
+  };
+};
+
+type Image = {
+  url: string;
+}
+const fetchImage = async (): Promise<Image> => {
   const res = await fetch("https://api.thecatapi.com/v1/images/search");
   const images = await res.json();
-  console.log(images);
+
   return images[0];
 }
 
