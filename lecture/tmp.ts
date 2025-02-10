6f042b3971511125aefaa311019753b839f46e64
ko <pleiades1424@gmail.com>
2025-02-10 12:55:07 +0900

    add: typescript練習用環境の構築

---
+++ lecture/tmp.ts
@@ -0,0 +1,8 @@
+const num1 = 10;
+const num2 = 10;
+console.log(num1 === num2);
+
+const symbol1 = Symbol("foo");
+const symbol2 = Symbol("foo");
+console.log(symbol1 === symbol1);
+console.log(symbol1 === symbol2);
